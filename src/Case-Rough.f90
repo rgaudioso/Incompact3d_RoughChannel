10bd28c57d46776fd73c976b15e7daab24475df8
Riccardo Gaudioso <riccardo.gaudioso@tu-braunschweig.de>
2024-07-30 16:43:27 +0200

    Updated ohi BC for Case-Rough.f90

--- src/Case-Rough.f90
+++ src/Case-Rough.f90
@@ -77,14 +77,6 @@ contains
        !  enddo
        !enddo
 
-       !phi1(:,:,:,:) = zero !change as much as you want
-       !if ((nclyS1 == 2).and.(xstart(2) == 1)) then
-       !  !! Generate a hot patch on bottom boundary
-       !  phi1(:,1,:,:) = one
-       !endif
-       !if ((nclySn == 2).and.(xend(2) == ny)) then
-       !  phi1(:,xsize(2),:,:) = zero
-       !endif
        !if (nrank==0.and.(mod(itime, ilist) == 0 .or. itime == ifirst .or. itime == ilast)) then
        !   write(*,*) 'Imposing quadratic (Poiseuille-like) temperature profile'
        !endif
@@ -114,7 +106,7 @@ contains
                if (istret/=0) y=yp(j+xstart(2)-1)
                do i=1,xsize(1)
                   if (ep1(i,j,k).eq.0) then
-                     phi1(i,j,k,:) = zero !one                
+                     phi1(i,j,k,:) = one !zero                
                   else
                      phi1(i,j,k,:) = zero
                   endif
@@ -122,6 +114,14 @@ contains
             enddo
          enddo
        enddo
+       !phi1(:,:,:,:) = zero !change as much as you want
+       if ((nclyS1 == 2).and.(xstart(2) == 1)) then
+       !  !! Generate a hot patch on bottom boundary
+          phi1(:,1,:,:) = zero !one
+       endif
+       if ((nclySn == 2).and.(xend(2) == ny)) then
+          phi1(:,xsize(2),:,:) = zero
+       endif
     endif  
    !+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
    !++++++++++++++++++++++++++++++++++++INIT FLOW VEL++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -180,7 +180,7 @@ contains
        enddo
     elseif (iin==3) then ! Init to turbulent flows using random numbers (ONLY CENTERLINE) + lam profile
        if (nrank==0.and.(mod(itime, ilist) == 0 .or. itime == ifirst .or. itime == ilast)) then
-           write(*,*) 'Imposing Poiseuille vel. profile with centerline noise'
+           write(*,*) 'Imposing uniform vel. profile with modulated noise' !'Imposing Poiseuille vel. profile with centerline noise'
        end if
        call system_clock(count=code)
        if (iin.eq.3) code=0
@@ -196,17 +196,17 @@ contains
              if (istret==0) y=real(j+xstart(2)-1-1,mytype)*dy-yly*half
              if (istret/=0) y=yp(j+xstart(2)-1)-yly*half
              !um=exp(-zptwo*y*y)
-             um=exp(-ten*y*y)
+             um=exp(-two*y*y)
              do i=1,xsize(1)
                 if (idir_stream == 1) then
                    if (ep1(i,j,k).eq.0) then
                       if (y.lt.(yly/2+0.5*yly/2).or.y.ge.(yly/2-0.5*yly/2)) then
                          !Poiseuille flow (nondim) => u(y) = 1 - y*y
-                         ux1(i,j,k)=init_noise*um*(two*ux1(i,j,k)-one)+(one-y*y)
+                         ux1(i,j,k)=init_noise*um*(two*ux1(i,j,k)-one)+one !(one-y*y)
                          uy1(i,j,k)=init_noise*um*(two*uy1(i,j,k)-one)
                          uz1(i,j,k)=init_noise*um*(two*uz1(i,j,k)-one)
                       else !Avoid noise close to walls
-                         ux1(i,j,k)=one-y*y
+                         ux1(i,j,k)=one !-y*y
                          uy1(i,j,k)=zero
                          uz1(i,j,k)=zero
                       endif
